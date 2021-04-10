3a67ff3a2b83a28ec5617afa1f7f0aff1b24f1a5
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2021-04-10 12:25:02 +0530

    second commit

---
+++ lib/homePage.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:merlin/authentication_service.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("HOME"),
+            ElevatedButton(
+              onPressed: () {
+                context.read<AuthenticationService>().signOut();
+              },
+              child: Text("Sign out"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signInPage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:merlin/authentication_service.dart';
+import 'package:provider/provider.dart';
+
+class SignInPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: "Email",
+            ),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: InputDecoration(
+              labelText: "Password",
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthenticationService>().signIn(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  );
+            },
+            child: Text("Sign in"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthenticationService>().signUp(
+                    email: emailController.text.trim(),
+                    password: passwordController.text.trim(),
+                  );
+            },
+            child: Text("Sign Up"),
+          )
+        ],
+      ),
+    );
+  }
+}
