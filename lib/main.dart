2377486d931cef85647496407b48e14dcfd2d064
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-04-10 23:09:00 +0530

    main auth page placed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:merlin/auth/mainAuthPage.dart';
 import 'package:merlin/authentication_service.dart';
 import 'package:merlin/homePage.dart';
 import 'package:merlin/mainapp.dart';
@@ -29,8 +30,10 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Confluence',
+        debugShowCheckedModeBanner: false,
         theme: buildThemeData(context),
-        home: MainApp(),
+        // home: MainApp(),
+        home: MainAuthPage(),
         // home: AuthenticationWrapper(),
       ),
     );
