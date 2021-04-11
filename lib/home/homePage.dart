60061cfd80c64ff16ee13e1f515ff10d1b4fdf68
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-04-11 08:46:37 +0530

    added hacks

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -21,29 +21,59 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Container(
-        child: ListView.separated(
-          separatorBuilder: (_, index) {
-            return Divider();
+          // child: ListView.separated(
+          //   separatorBuilder: (_, index) {
+          //     return Divider();
+          //   },
+          //   itemCount: 10,
+          //   itemBuilder: (_, index) {
+          //     return ListTile(
+          //       onTap: () {
+          //         Navigator.push(context,
+          //             MaterialPageRoute(builder: (context) => HackDetailPage()));
+          //       },
+          //       contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          //       leading: ClipRRect(
+          //         borderRadius: BorderRadius.circular(8.0),
+          //         child: Image.asset("assets/placeholder.png"),
+          //       ),
+          //       title: Text("hackathon name"),
+          //       subtitle: Text("12 april 2021"),
+          //       trailing: Icon(Feather.chevron_right),
+          //     );
+          //   },
+          // ),
+          child: ListView(children: [
+        ListTile(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => HackDetailPage()));
           },
-          itemCount: 10,
-          itemBuilder: (_, index) {
-            return ListTile(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HackDetailPage()));
-              },
-              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.asset("assets/placeholder.png"),
-              ),
-              title: Text("hackathon name"),
-              subtitle: Text("12 april 2021"),
-              trailing: Icon(Feather.chevron_right),
-            );
-          },
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network("https://hackofiesta-iiitl.tech/img/logo.png"),
+          ),
+          title: Text("HACKOFIESTA"),
+          subtitle: Text("16 april 2021 - 18 april 2021"),
+          trailing: Icon(Feather.chevron_right),
         ),
-      ),
+        ListTile(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => HackDetailPage()));
+          },
+          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+                "https://hackthespace.netlify.app/assets/img/logo111.PNG"),
+          ),
+          title: Text("Hack The Space 🚀"),
+          subtitle: Text("17 april 2021 - 19 april 2021"),
+          trailing: Icon(Feather.chevron_right),
+        ),
+      ])),
     );
   }
 }
