ff7b01a5a29bf13c71eae3017dbb2feef193d87a
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-04-11 07:21:20 +0530

    refined hack details page

---
+++ lib/home/recruitment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecruitmentPage extends StatefulWidget {
+  @override
+  _RecruitmentPageState createState() => _RecruitmentPageState();
+}
+
+class _RecruitmentPageState extends State<RecruitmentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
