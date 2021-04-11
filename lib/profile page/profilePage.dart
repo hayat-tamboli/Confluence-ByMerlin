93559331ab680d4cd5c5e6a288811eda8c199f1c
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2021-04-11 08:06:25 +0530

    next commit

--- lib/profile page/profilePage.dart
+++ lib/profile page/profilePage.dart
@@ -25,95 +25,95 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
       body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          children: [
-            SizedBox(height: 40.0),
-            CircleAvatar(
-              radius: 80.0,
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(100.0))),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              "Hayat Tamboli",
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              "VIT' 23",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300),
-            ),
-            SizedBox(height: 40.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset("assets/google.png", height: 50.0, width: 50.0),
-                Image.asset("assets/linkedin.png", height: 50.0, width: 50.0),
-                Image.asset("assets/twitter.png", height: 50.0, width: 50.0),
-              ],
-            ),
-            SizedBox(height: 40.0),
-            Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  child: Center(child: Text("Java")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 70.0,
-                  child: Center(child: Text("C++")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[20],
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 100.0,
-                  child: Center(child: Text("Flask")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[20],
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-              ],
-            ),
-            SizedBox(height: 40.0),
-            Row(
-              children: [
-                Container(
-                  width: 100.0,
-                  child: Center(child: Text("Flutter")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[20],
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 100.0,
-                  child: Center(child: Text("Firebase")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[20],
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-                SizedBox(width: 20.0),
-                Container(
-                  width: 100.0,
-                  child: Center(child: Text("Javascript")),
-                  decoration: BoxDecoration(
-                      color: Colors.grey[20],
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-              ],
-            ),
-          ],
-        ),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          SizedBox(height: 40.0),
+          CircleAvatar(
+            radius: 80.0,
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(100.0))),
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            "Hayat Tamboli",
+            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            "VIT' 23",
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300),
+          ),
+          SizedBox(height: 40.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("assets/google.png", height: 50.0, width: 50.0),
+              Image.asset("assets/linkedin.png", height: 50.0, width: 50.0),
+              Image.asset("assets/twitter.png", height: 50.0, width: 50.0),
+            ],
+          ),
+          SizedBox(height: 40.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                child: Center(child: Text("Java")),
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+              SizedBox(width: 20.0),
+              Container(
+                width: 70.0,
+                child: Center(child: Text("C++")),
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+              SizedBox(width: 20.0),
+              Container(
+                width: 100.0,
+                child: Center(child: Text("Flask")),
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ],
+          ),
+          SizedBox(height: 40.0),
+          Row(
+            children: [
+              Container(
+                width: 100.0,
+                child: Center(child: Text("Flutter")),
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+              SizedBox(width: 20.0),
+              Container(
+                width: 100.0,
+                child: Center(child: Text("Firebase")),
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+              SizedBox(width: 20.0),
+              Container(
+                width: 100.0,
+                child: Center(child: Text("Javascript")),
+                decoration: BoxDecoration(
+                    color: Colors.grey[20],
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ],
+          ),
+        ],
       ),
+    ),
     );
   }
 }
