ea31dad7c0e596f96291c459021ed7f27ebf5c42
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2021-04-13 14:53:22 +0530

    added info to tinder cards

--- lib/swipe.dart
+++ lib/swipe.dart
@@ -28,8 +28,282 @@ class ExampleHomePage extends StatefulWidget {
 class _ExampleHomePageState extends State<ExampleHomePage>
     with TickerProviderStateMixin {
   List<Widget> welcomeImages = [
-    SwipeCard(),
-    SwipeCard(),
+    Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(height: 10.0),
+            CircleAvatar(
+              radius: 30.0,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(100.0))),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Hayat Tamboli",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "VIT' 23",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("assets/google.png", height: 50.0, width: 50.0),
+                Image.asset("assets/linkedin.png", height: 50.0, width: 50.0),
+                Image.asset("assets/twitter.png", height: 50.0, width: 50.0),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Java")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("C++")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Flask")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Flutter")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Firebase")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Javascript")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+    Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(height: 10.0),
+            CircleAvatar(
+              radius: 30.0,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      borderRadius: BorderRadius.circular(100.0))),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Tanmay Agrawal",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "VIT' 22",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("assets/google.png", height: 50.0, width: 50.0),
+                Image.asset("assets/linkedin.png", height: 50.0, width: 50.0),
+                Image.asset("assets/twitter.png", height: 50.0, width: 50.0),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Python")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("C++")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("HTML")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Flutter")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Firebase")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Ruby")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+    Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(height: 10.0),
+            CircleAvatar(
+              radius: 30.0,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(100.0))),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Hayat Tamboli",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "VIT' 23",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("assets/google.png", height: 50.0, width: 50.0),
+                Image.asset("assets/linkedin.png", height: 50.0, width: 50.0),
+                Image.asset("assets/twitter.png", height: 50.0, width: 50.0),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Java")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("C++")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Flask")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Flutter")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Firebase")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+                SizedBox(width: 10.0),
+                Container(
+                  width: 70.0,
+                  child: Center(child: Text("Javascript")),
+                  decoration: BoxDecoration(
+                      color: Colors.grey[20],
+                      borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
     SwipeCard(),
     SwipeCard(),
     SwipeCard(),
@@ -41,7 +315,8 @@ class _ExampleHomePageState extends State<ExampleHomePage>
     CardController controller; //Use this to trigger swap.
 
     return new Scaffold(
-      appBar: AppBar(leading: GestureDetector(
+      appBar: AppBar(
+          leading: GestureDetector(
               child: Icon(Icons.arrow_back_ios),
               onTap: () {
                 Navigator.pop(context);
